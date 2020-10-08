c456c9480b329612e2806e921c2cf8768f69ad43
Renaud Gaspard <gaspardrenaud@hotmail.com>
2020-10-08 16:37:40 +0200

    fixes other issue with temperature conversion

--- octoprint_resource_monitor/temperature/__init__.py
+++ octoprint_resource_monitor/temperature/__init__.py
@@ -1,7 +1,7 @@
 def to_fahrenheit(celsius):
 	result = dict()
 	for key, val in celsius.items():
-		if not isinstance(val, str):
+		if val is not None and not isinstance(val, str):
 			result[key] = val * 1.8 + 32
 		else:
 			result[key] = val
